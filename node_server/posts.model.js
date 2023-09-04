8dd600c7b2694f43105aa30a2ee6d61f27766671
ahmedyousry0098 <ahmedyousry098@gmail.com>
2023-09-04 13:57:48 +0300

    add node_http task

---
+++ node_server/posts.model.js
@@ -0,0 +1,19 @@
+const posts = [
+    {
+        content: "Hello there!",
+        likes: 25,
+        comments: 8
+    },
+    {
+        content: "Hi there!",
+        likes: 50,
+        comments: 4
+    },
+    {
+        content: "Hello Word!",
+        likes: 41,
+        comments: 70
+    },
+]
+
+module.exports = {posts}
